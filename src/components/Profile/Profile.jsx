d89acfcb37995e883577e8d3069bb7e8f7a60a5f
Lora <lorat1403@gmail.com>
2022-09-13 19:53:12 +0300

    props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,6 +9,8 @@ import {
   ItemNum,
 } from 'components/Profile/Profile.styled';
 
+import PropTypes from 'prop-types';
+
 export const Profile = ({ avatar, username, tag, location, stats }) => {
   return (
     <Container>
@@ -35,4 +37,16 @@ export const Profile = ({ avatar, username, tag, location, stats }) => {
       </List>
     </Container>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
